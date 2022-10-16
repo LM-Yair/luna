b948e62b213f4f3904c7f6bb54f48a17b5f71db7
LM-Yair <yairlazaro@outlook.com>
2022-10-16 15:28:40 -0500

    delete: index files because it was getting entire database

--- pages/api/statuses/interactions/comments/index.js
+++
@@ -1,8 +0,0 @@
-import Comments from "db/comments";
-
-const comments = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).send(Comments);
-};
-
-export default comments;
--- pages/api/statuses/interactions/tweets/index.js
+++
@@ -1,8 +0,0 @@
-import Interactions from "db/interactions";
-
-const interactions = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).send(Interactions);
-};
-
-export default interactions;
