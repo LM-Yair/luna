e71080f762fbc779aacb01de9be75cd0454922e9
LM-Yair <yairlazaro@outlook.com>
2022-10-17 14:09:08 -0500

    add: priority prop

--- components/Icons/Logo.js
+++ components/Icons/Logo.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
-const Logo = ({ size = 30 }) => {
+const Logo = ({ size = 30, priorityimg = false }) => {
   return (
     <Image
       className="bg-neutral-800 rounded-full"
       width={size}
       height={size}
+      priority={priorityimg}
       src="/logo.png"
       alt="icon"
     />
