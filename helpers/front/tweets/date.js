45e6c5ee234f6e32d3509e38e1f6e726def0a235
LM-Yair <yairlazaro@outlook.com>
2022-10-18 19:06:38 -0500

    edit: function to get tweet date

--- helpers/front/tweets/date.js
+++ helpers/front/tweets/date.js
@@ -1,43 +1,23 @@
-const getMonth = (monthNumber) => {
-  const months = [
-    "jan",
-    "feb",
-    "mar",
-    "apr",
-    "may",
-    "jun",
-    "jul",
-    "aug",
-    "sep",
-    "oct",
-    "nov",
-    "dec",
-  ];
-  return months[monthNumber];
+const DATE_UNITS = [
+  ["day", 86400],
+  ["hour", 3600],
+  ["minute", 60],
+  ["second", 1],
+];
+
+const getDateDiff = (timestamp) => {
+  const now = Date.now();
+  const elapsed = (timestamp - now) / 1000;
+  for (const [unit, secondsInUnit] of DATE_UNITS) {
+    if (Math.abs(elapsed) > secondsInUnit || unit === "second") {
+      const value = Math.round(elapsed / secondsInUnit);
+      return { value, unit };
+    }
+  }
 };
-export const getDate = (tweetDate = {}) => {
-  const currentDate = {
-    seconds: new Date().getSeconds(),
-    minutes: new Date().getMinutes(),
-    hours: new Date().getHours(),
-    day: new Date().getDate(),
-    month: new Date().getMonth(),
-    year: new Date().getFullYear(),
-  };
-  if (currentDate.year !== tweetDate.year) {
-    return `${tweetDate.day} ${getMonth(tweetDate.month)} ${tweetDate.year}`;
-  }
-  if (
-    currentDate.month !== tweetDate.month ||
-    currentDate.day !== tweetDate.day
-  ) {
-    return `${tweetDate.day} ${getMonth(tweetDate.month)}`;
-  }
-  if (currentDate.hours !== tweetDate.hours) {
-    return `${currentDate.hours - tweetDate.hours}h`;
-  }
-  if (currentDate.minutes !== tweetDate.minutes) {
-    return `${currentDate.minutes - tweetDate.minutes}min`;
-  }
-  return `${currentDate.seconds - tweetDate.seconds}s`;
+
+export const timeAgo = (tweetDate) => {
+  const { value, unit } = getDateDiff(tweetDate);
+  const rtf = new Intl.RelativeTimeFormat("en", { style: "short" });
+  return rtf.format(value, unit);
 };
