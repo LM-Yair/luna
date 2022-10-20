fff280cbfc42739cf21b5577c272a206eaf24df9
LM-Yair <yairlazaro@outlook.com>
2022-10-19 21:04:49 -0500

    edit: icons style

--- components/Icons/Home.js
+++ components/Icons/Home.js
@@ -1,17 +1,19 @@
 const HomeIcon = ({ size = 25 }) => {
+  const centerContent = (size - 2) / 8;
+  const scale = '0.9';
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 251.479 251.479"
-      width={size}
-      height={size}
-      style={{
-        enableBackground: "new 0 0 251.479 251.479",
-      }}
-      xmlSpace="preserve"
-    >
-      <path d="M209.355 135.551a7.5 7.5 0 0 0-7.5 7.5v76.109H49.634v-76.109a7.5 7.5 0 0 0-7.5-7.5 7.5 7.5 0 0 0-7.5 7.5v83.609a7.5 7.5 0 0 0 7.5 7.5h167.221a7.5 7.5 0 0 0 7.5-7.5v-83.609a7.5 7.5 0 0 0-7.5-7.5z" />
-      <path d="M249.282 137.748 131.035 19.515a7.5 7.5 0 0 0-10.606 0L2.197 137.748a7.5 7.5 0 0 0 5.304 12.803c1.92 0 3.839-.732 5.304-2.197l112.929-112.93 112.943 112.93a7.5 7.5 0 0 0 10.605-10.606z" />
+    <svg width={size} height={size} xmlns="http://www.w3.org/2000/svg">
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        transform={`translate(${centerContent},${centerContent}) scale(${scale})`}
+      >
+        <path d="m1.5 10.5 9-9 9 9" />
+        <path d="M3.5 8.5v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-7" />
+      </g>
     </svg>
   );
 };
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,31 +1,26 @@
 import Link from "next/link";
 
 import HomeIcon from "components/Icons/Home";
-import MessageIcon from "components/Icons/Message";
-import NotificationIcon from "components/Icons/Notification";
 import SearchIcon from "components/Icons/Search";
+import CreateTweet from "components/Icons/CreateTweet";
 
 const Navigation = () => {
+  const iconClass = "p-2 hover:bg-blue-300/75 rounded-full";
   return (
-    <nav className="p-2 h-12 w-full bg-neutral-200 flex justify-evenly items-center sticky left-0 bottom-0 md:bottom-0">
+    <nav className="p-2 h-12 w-full bg-neutral-200 flex justify-around items-center sticky left-0 bottom-0 md:bottom-[-1px]">
       <Link href="/home">
-        <a className="p-2">
+        <a className={iconClass}>
           <HomeIcon size={25} />
         </a>
       </Link>
       <Link href="/home">
-        <a className="p-2">
+        <a className={iconClass}>
           <SearchIcon size={25} />
         </a>
       </Link>
-      <Link href="/home">
-        <a className="p-2">
-          <NotificationIcon size={25} />
-        </a>
-      </Link>
-      <Link href="/home">
-        <a className="p-2">
-          <MessageIcon size={25} />
+      <Link href="/compose/tweet">
+        <a className={iconClass}>
+          <CreateTweet size={25} />
         </a>
       </Link>
     </nav>
