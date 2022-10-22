1043c185f2610f801ea049a0f24687868ab7795b
LM-Yair <yairlazaro@outlook.com>
2022-10-22 13:26:05 -0500

    add: useRouter to function go to back

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,20 +1,30 @@
-const Header = ({ btn = false, onClick, pageName = "" }) => {
+import ArrowLeft from "components/Icons/ArrowLeft";
+import { useRouter } from "next/router";
+
+const Header = ({ pageName = "" }) => {
+  const router = useRouter();
+  // const goToBack = () => router.back();
+  const goToBack = () => {
+    const urls = Object.keys( router.components );
+    if(urls.length === 0){
+      router.replace('/');
+    }
+    router.back();
+  };
   return (
     <header className="p-2 sticky top-0 left-0 flex justify-between bg-neutral-200/75 backdrop-blur-sm">
-      <div className="w-16 flex items-center justify-center">{"<-"}</div>
+      <div className="flex items-center justify-center">
+        <div
+          className="p-2 rounded-full cursor-pointer hover:bg-neutral-400/50"
+          onClick={goToBack}
+        >
+          <ArrowLeft />
+        </div>
+      </div>
       <div className="w-full flex items-center">
-        <h2 className="text-xl font-bold ml-2">{pageName}</h2>
+        <h2 className="text-xl ml-2">{pageName}</h2>
       </div>
-      <div className="w-16 flex items-center justify-center">
-        {button && (
-          <button
-            className="text-neutral-300 p-2 bg-neutral-800"
-            onClick={onClick}
-          >
-            {btn}
-          </button>
-        )}
-      </div>
+      <div className="w-16 flex items-center justify-center"> </div>
     </header>
   );
 };
