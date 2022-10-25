b08ac7b5d903b961b62537f77a97fa49169a2aed
LM-Yair <yairlazaro@outlook.com>
2022-10-24 22:39:13 -0500

    edit: extracted firebase config to another file

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAq4LuYWQjEdCAnvaERkeNbEinJLKypAWw",
+  authDomain: "luna-85820.firebaseapp.com",
+  projectId: "luna-85820",
+  storageBucket: "luna-85820.appspot.com",
+  messagingSenderId: "764355860670",
+  appId: "1:764355860670:web:00cd9f53df6d9c0e9e26ef",
+  measurementId: "G-HVBHJWSMK1",
+};
