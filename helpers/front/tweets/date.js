4066566f94c43ce60f83c65e296e6605657b00f4
LM-Yair <yairlazaro@outlook.com>
2022-10-25 15:36:16 -0500

    add: support to old browsers

--- helpers/front/tweets/date.js
+++ helpers/front/tweets/date.js
@@ -5,6 +5,9 @@ const DATE_UNITS = [
   ["second", 1],
 ];
 
+const isDateTimeformatSupported =
+  typeof Intl !== "undefined" && Intl.DateTimeFormat;
+
 const getDateDiff = (timestamp) => {
   const now = Date.now();
   const elapsed = (timestamp - now) / 1000;
@@ -16,8 +19,26 @@ const getDateDiff = (timestamp) => {
   }
 };
 
+const alternativeTime = (timestamp, config) => {
+  const date = new Date(timestamp);
+  return date.toLocaleDateString("en", config);
+};
+
 export const timeAgo = (tweetDate) => {
+  const config = {
+    style: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: 'numeric',
+    hour12: false,
+  };
+  if (!isDateTimeformatSupported) {
+    return alternativeTime(tweetDate, config);
+  }
   const { value, unit } = getDateDiff(tweetDate);
-  const rtf = new Intl.RelativeTimeFormat("en", { style: "short" });
+  const rtf = new Intl.RelativeTimeFormat("en", config);
   return rtf.format(value, unit);
 };
