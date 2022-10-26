3c5b6ba9db20e4676bdc4ee6297841edba2576f4
LM-Yair <yairlazaro@outlook.com>
2022-10-25 23:10:08 -0500

    edit: state management to disable button, while there is no message, image or the image is loading, it will be disable

--- components/CreateTweet/ButtonToSend.js
+++ components/CreateTweet/ButtonToSend.js
@@ -1,6 +1,15 @@
+import { IMAGE_STATE } from "CONSTANTS/IMAGE_STATES";
 
 const ButtonSend = ({ form }) => {
-  if (form.message.trim().length > 0 || form.image.status ) {
+  const { message, image } = form;
+  const hasMessage = message.trim().length > 0;
+  const hasImage = ![
+    IMAGE_STATE.IS_LOADING,
+    IMAGE_STATE.NOT_IMG,
+    IMAGE_STATE.ERROR,
+  ].includes(image.status);
+
+  if (hasMessage || hasImage) {
     return (
       <input
         className="text-neutral-300 py-2 px-4 bg-neutral-800 rounded-xl cursor-pointer"
