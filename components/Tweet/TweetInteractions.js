4b83927e36023a2dd9c3b2caebea56c6126d692d
LM-Yair <yairlazaro@outlook.com>
2022-10-29 17:49:44 -0500

    edit: commented out unnecessary code

--- components/Tweet/TweetInteractions.js
+++ components/Tweet/TweetInteractions.js
@@ -2,28 +2,27 @@ import Comment from "components/Icons/Comment";
 import Like from "components/Icons/Like";
 import ReTweet from "components/Icons/Retweet";
 import Send from "components/Icons/Send";
-import { useEffect, useState } from "react";
 
-const TweetInteractions = ({ id }) => {
-  const [interactions, setInteractions] = useState(null);
-  useEffect(() => {
-    fetch(`/api/statuses/interactions/tweets/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setInteractions(data.interaction);
-      })
-      .catch(console.log);
-  }, []);
-  if (!interactions) {
-    return (
-      <div className="py-2 flex justify-between">
-        <Comment size={20} />
-        <Like size={20} />
-        <ReTweet size={20} />
-        <Send size={20} />
-      </div>
-    );
-  }
+const TweetInteractions = () => {
+  // const [interactions, setInteractions] = useState(null);
+  // useEffect(() => {
+  //   fetch(`/api/statuses/interactions/tweets/${id}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setInteractions(data.interaction);
+  //     })
+  //     .catch(console.log);
+  // }, []);
+  // if (!interactions) {
+  //   return (
+  //     <div className="py-2 flex justify-between">
+  //       <Comment size={20} />
+  //       <Like size={20} />
+  //       <ReTweet size={20} />
+  //       <Send size={20} />
+  //     </div>
+  //   );
+  // }
   return (
     <div className="py-2 flex justify-between">
       <Comment count={interactions.comments} size={20} />
