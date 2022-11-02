d30b5bfa6c66f9b93e27aaac45a3879cd2cd7b6f
LM-Yair <yairlazaro@outlook.com>
2022-11-02 03:14:07 -0600

    edit: main tag class

--- components/ContainerPage/ContainerPage.js
+++ components/ContainerPage/ContainerPage.js
@@ -1,13 +1,23 @@
 import { containerPage } from "./containerPage.module.css";
 
 const ContainerPage = ({ children }) => {
+  const mainTagClass = [
+    "h-full",
+    "w-full",
+    "max-w-md",
+    "relative",
+    "overflow-x-hidden",
+    "bg-neutral-200",
+    "md:max-w-[25rem]",
+    "md:shadow-xl",
+    "md:rounded-xl",
+    "md:max-h-[40rem]",
+  ].join(" ");
   return (
     <div
       className={`h-screen bg-neutral-100 flex items-center justify-center overflow-hidden ${containerPage}`}
     >
-      <main className="h-full w-full max-w-md relative overflow-x-hidden bg-neutral-200 md:max-w-[25rem] md:shadow-xl md:rounded-xl md:max-h-[40rem]">
-        {children}
-      </main>
+      <main className={mainTagClass}>{children}</main>
     </div>
   );
 };
